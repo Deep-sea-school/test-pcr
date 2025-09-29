aae5282436d398e1baebc8311e8bd1522262d658
Deep-sea-school <Deep-sea-school@users.noreply.github.com>
2025-09-29 02:45:44 +0000

    deploy: c65a3dc2c3af8cc86aa3aa250a2e1e72ce1848c7

--- js/packager-options-ui.js
+++ js/packager-options-ui.js
@@ -9614,7 +9614,7 @@ cd "$(dirname "$0")"
       version: this.options.app.version,
       description: 'A packaged Scratch project',
       main: 'index.html',
-      author: 'TurboWarp Packager',
+      author: '0.2Studio',
       license: 'MIT',
       dependencies: {
         'cordova-android': '^10.1.1'
@@ -9623,7 +9623,7 @@ cd "$(dirname "$0")"
         platforms: ['android']
       },
       scripts: {
-        "build": "cordova build android",
+        "build": "cordova build android --verbose -- --no-build-cache",
         "prepare": "cordova prepare android"
       }
     };
